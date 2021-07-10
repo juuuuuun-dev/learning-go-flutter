1a0c904f0678b361ac3a891fbf78027059ac25e0
junkata <junkata.dev@gmail.com>
2021-07-10 20:29:09 +0900

    fix typo

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -16,46 +16,65 @@ class AppDemo extends StatefulWidget {
 
 class _AppDemoState extends State<AppDemo> {
   // late AnimationController _controller;
-  double fetchCountPercentage = 10
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        theme: ThemeData.light(),
+        darkTheme: ThemeData.dark(),
         home: Scaffold(
-            backgroundColor: Colors.blueGrey,
+            backgroundColor: Colors.grey[100],
             body: SizedBox.expand(
-              child: stack(
+              child: Stack(
                 children: [
                   FutureBuilder<List<Product>>(
-                    future: fetchFromServer(),
-                    builder: (BuildContext context, AsyncSnapshop snapshop) {
-                      if (snapshop.connectionState == ConnectionState.waiting) {
-                        return Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      }
-                      if (snapshot.hasError) {
-                        return Center(
-                          child: Text("${snapshop.error}", style:TextStyle(color: Colors.red)),
-                        )
-                      }
-                    },
-                  )
+                      future: fetchFromServer(),
+                      builder: (BuildContext context, AsyncSnapshot snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+                        if (snapshot.hasError) {
+                          return Center(
+                            child: Text("${snapshot.error}",
+                                style: TextStyle(color: Colors.red)),
+                          );
+                        }
+                        if (snapshot.hasData) {
+                          return ListView.builder(
+                            itemCount: snapshot.data.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Card(
+                                  child: ListTile(
+                                title: Text(snapshot.data[index].name),
+                                subtitle: Text(
+                                    "Count: ${snapshot.data[index].count} \t Price: ${snapshot.data[index].price}"),
+                              ));
+                            },
+                          );
+                        }
+                        return Container();
+                      })
                 ],
               ),
             )));
   }
-  Future<list<Product>> fetchFromServer() async {
-    var url = "http://127.0.0.1:5500/products/${fetchCountPercentage}";
-    var response = await http.get(url);
+
+  Future<List<Product>> fetchFromServer() async {
+    var fetchCountPercentage = 110;
+    var url = "http://192.168.11.2:5500/products/${fetchCountPercentage}";
+    var response = await http.get(Uri.parse(url));
     List<Product> productList = [];
-    if (response.status == 200) {
-      var productMap = covert.jsonDecode(response.body);
-      for (final item in producMap) {
+    if (response.statusCode == 200) {
+      debugPrint("${fetchCountPercentage.round()}");
+      var productMap = convert.jsonDecode(response.body);
+      for (final item in productMap) {
         productList.add(Product.fromJson(item));
       }
     }
-    return prodcutList;
+    return productList;
   }
   // @override
   // void initState() {
--- flutter/lib/productModel.dart
+++ flutter/lib/productModel.dart
@@ -1,11 +1,11 @@
 class Product {
   String name;
-  double price;
+  num price;
   int count;
 
   Product(this.name, this.price, this.count);
-  Product.fromJson(Map<String,dynamic> json)
-   : name = json["Name"],
-   price = json["Price"],
-   count = json["Count"]
+  Product.fromJson(Map<String, dynamic> json)
+      : name = json["Name"],
+        price = json["Price"],
+        count = json["Count"];
 }
